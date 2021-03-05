9b829741ab278e6c6ed1a9f9e99a6714e5d29423
andrey-programmer <Lak0sta1302@gmail.com>
2021-03-05 17:22:44 +0300

    Подготовил вёрстку и раскидал по компонентам Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import AuthLayout from '@/layouts/AuthLayout'
+  import MainLayout from '@/layouts/MainLayout'
+  export default {
+    components: {
+      AuthLayout,
+      MainLayout
+    },
+    computed: {
+      layout() {
+        // console.log(this.$route.meta.layout)
+        return this.$route.meta.layout || 'authLayout'
+      }
     }
   }
-}
+</script>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,73 @@ import Home from '../views/Home.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    meta: {
+      layout: 'authLayout'
+    },
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {
+      layout: 'mainLayout'
+    },
+    component: () => import('@/views/Categories.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'mainLayout'
+    },
+    component: () => import('@/views/History.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {
+      layout: 'mainLayout'
+    },
+    component: () => import('@/views/Detail.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'mainLayout'
+    },
+    component: () => import('@/views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'mainLayout'
+    },
+    component: () => import('@/views/Profile.vue')
+  },
+  {
+    path: '/records',
+    name: 'records',
+    meta: {
+      layout: 'mainLayout'
+    },
+    component: () => import('@/views/Records.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {
+      layout: 'authLayout'
+    },
+    component: () => import('@/views/Register.vue')
+  },
 ]
 
 const router = createRouter({
